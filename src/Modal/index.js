b20320989f6ef0b36ad598ed150335ca4da4f84f
Fraasi <doofassi@gmail.com>
2019-03-23 23:39:31 +0200

    finishing modal & form

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -4,9 +4,8 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import './index.css'
 
-
 ReactModal.setAppElement('#root')
-const customStyles = {
+const modalCustomStyles = {
   content: {
     top: '50%',
     left: '50%',
@@ -14,21 +13,25 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    width: '70vw',
-    height: '70vh'
+    width: '60vw',
+    height: '55vh',
+    overflow: 'visible'
   }
-};
+}
+
+class DateInputButton extends React.Component {
+  render() {
+    return (
+      <button
+        className="date-input-button"
+        onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
 
 export default class Modal extends Component {
-
-  componentDidMount() {
-    console.log('componentDidMount')
-
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount:')
-  }
 
   handleTitleChange = (newTitle) => {
     this.props.handleModalEventEdit('title', newTitle.target.value)
@@ -64,58 +67,76 @@ export default class Modal extends Component {
     return (
       <ReactModal
         isOpen={modalIsOpen}
-        style={customStyles}
+        style={modalCustomStyles}
         contentLabel="Example Modal"
         shouldCloseOnOverlayClick={true}
         onRequestClose={closeModal}
         closeTimeoutMS={150}
       >
-        <label htmlFor="id-num">Event id: ({id})</label>
-        <input type="text" value={title} onChange={this.handleTitleChange} placeholder="Title" required/>
-        <br />
-        <br />
-        <label htmlFor="start-time">Event start </label>
-        <DatePicker
-          selected={start}
-          onChange={this.handleStartDateChange}
-          required={true}
-          showTimeSelect={true}
-          showMonthDropdown={true}
-          showWeekNumbers={true}
-          shouldCloseOnSelect={false}
-          timeFormat="HH:mm"
-          timeIntervals={15}
-          dateFormat="dd/MM/yyyy H:mm"
-          timeCaption="time"
-        />
-        <br />
-        <br />
-        <label htmlFor="end-time">Event end &nbsp;</label>
-        <DatePicker
-          selected={end}
-          onChange={this.handleEndDateChange}
-          required={true}
-          showTimeSelect={true}
-          showMonthDropdown={true}
-          showWeekNumbers={true}
-          shouldCloseOnSelect={false}
-          timeFormat="HH:mm"
-          timeIntervals={15}
-          dateFormat="dd/MM/yyyy H:mm"
-          timeCaption="time"
-        />
+        <div className="form-wrapper">
+          <div>
+            <label htmlFor="id-num">Event id: ({id})</label>
+            <input type="text" value={title} onChange={this.handleTitleChange} placeholder="Title" required />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="start-time">Event start </label>
+            <DatePicker
+              customInput={<DateInputButton />}
+              selected={start}
+              onChange={this.handleStartDateChange}
+              required={true}
+              showTimeSelect={true}
+              showMonthDropdown={true}
+              showWeekNumbers={true}
+              shouldCloseOnSelect={false}
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="dd/MM/yyyy H:mm"
+              timeCaption="time"
+              popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: '-40px, 0px'
+                }
+              }}
+            />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="end-time">Event end &nbsp;</label>
+            <DatePicker
+              customInput={<DateInputButton />}
+              selected={end}
+              onChange={this.handleEndDateChange}
+              required={true}
+              showTimeSelect={true}
+              showMonthDropdown={true}
+              showWeekNumbers={true}
+              shouldCloseOnSelect={false}
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="dd/MM/yyyy H:mm"
+              timeCaption="time"
+              popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: '-40px, 0px'
+                }
+              }}
+            />
+          </div>
+            <br />
+          <div>
+            <textarea onChange={this.handleDescChange} value={desc} placeholder="description" />
+          </div>
+        </div>
 
-        <br />
-
-        <p>
-          <textarea onChange={this.handleDescChange} value={desc} placeholder="description" />
-        </p>
-
-
-        <button onClick={this.handleEditSave}>save</button>
-        { id && <button onClick={this.handleEditDelete}>delete event</button> }
-        <button onClick={closeModal}>cancel</button>
-        
+        <div className="modal-buttons">
+          {id && <button onClick={this.handleEditDelete}>delete event</button>}
+          <button onClick={closeModal}>cancel</button>
+          <button onClick={this.handleEditSave}>save</button>
+        </div>
       </ReactModal>
     )
   }
